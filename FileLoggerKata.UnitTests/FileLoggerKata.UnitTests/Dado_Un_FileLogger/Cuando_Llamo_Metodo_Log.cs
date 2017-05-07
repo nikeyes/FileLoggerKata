cc1d7bf02e9012bafd60b3bf779aab4bdfac8cdf
nikey <nikey@ELEVEN>
2017-05-07 16:04:58 +0200

    Req.1: Red

---
+++ FileLoggerKata.UnitTests/FileLoggerKata.UnitTests/Dado_Un_FileLogger/Cuando_Llamo_Metodo_Log.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileLoggerKata.UnitTests.Dado_Un_FileLogger
+{
+    [TestClass]
+    public class Cuando_Llamo_Metodo_Log
+    {
+        [TestMethod]
+        public void Entonces_Es_Obligatorio_Pasarle_Un_Parámetro_Message()
+        {
+            FileLogger _sut = new FileLogger();
+
+            _sut.Log("Mensaje Obligatorio");
+
+        }
+    }
+}
---
+++ FileLoggerKata.UnitTests/FileLoggerKata.UnitTests/Dado_Un_FileLogger/FileLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileLoggerKata.UnitTests.Dado_Un_FileLogger
+{
+    internal class FileLogger
+    {
+        internal void Log(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
